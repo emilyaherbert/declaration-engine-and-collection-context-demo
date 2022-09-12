9b05737a0b657b307862b65472b3f2d70ca29c31
emilyaherbert <emily.herbert@fuel.sh>
2022-09-12 16:33:50 -0500

    Remove usused code in GraphNode.

--- de_cc/src/collection_context/graph_node.rs
+++ de_cc/src/collection_context/graph_node.rs
@@ -1,5 +1,5 @@
 use crate::{
-    language::ty::{typed_declaration::TyDeclaration, TyApplication, TyFile, TyNode},
+    language::ty::{TyApplication, TyFile, TyNode},
     type_system::type_mapping::TypeMapping,
     types::{copy_types::CopyTypes, pretty_print::PrettyPrint},
 };
@@ -11,7 +11,6 @@ pub(crate) enum GraphNode {
     Application(TyApplication),
     File(TyFile),
     Node(TyNode),
-    Declaration(TyDeclaration),
 }
 
 impl PrettyPrint for GraphNode {
@@ -20,7 +19,6 @@ impl PrettyPrint for GraphNode {
             GraphNode::Application(node) => node.pretty_print(cc),
             GraphNode::File(node) => node.pretty_print(cc),
             GraphNode::Node(node) => node.to_string(),
-            GraphNode::Declaration(node) => node.to_string(),
         }
     }
 }
@@ -31,7 +29,6 @@ impl CopyTypes for GraphNode {
             GraphNode::Application(node) => node.copy_types(cc, type_mapping),
             GraphNode::File(node) => node.copy_types(cc, type_mapping),
             GraphNode::Node(node) => node.copy_types(cc, type_mapping),
-            GraphNode::Declaration(node) => node.copy_types(cc, type_mapping),
         }
     }
 }
@@ -54,12 +51,6 @@ impl From<TyNode> for GraphNode {
     }
 }
 
-impl From<TyDeclaration> for GraphNode {
-    fn from(node: TyDeclaration) -> Self {
-        GraphNode::Declaration(node)
-    }
-}
-
 impl GraphNode {
     pub(crate) fn expect_application(&self) -> Result<&TyApplication, String> {
         match self {
@@ -78,13 +69,6 @@ impl GraphNode {
     pub(crate) fn expect_node(&self) -> Result<&TyNode, String> {
         match self {
             GraphNode::Node(node) => Ok(node),
-            _ => Err("did not expect to find this declaration".to_string()),
-        }
-    }
-
-    pub(crate) fn expect_declaration(&self) -> Result<&TyDeclaration, String> {
-        match self {
-            GraphNode::Declaration(node) => Ok(node),
             _ => Err("did not expect to find this declaration".to_string()),
         }
     }
